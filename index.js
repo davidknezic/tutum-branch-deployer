62083f11d14ca57ec09292878bcc04af5a81fcd6
David Knezić <davidknezic@gmail.com>
2015-12-27 01:56:34 +0100

    Introduce faked remove-event handling

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import createTutumEvents from './lib/tutum-events'
 import colors from 'colors'
 import Bacon from 'baconjs'
 import deployToTutum from './lib/deploy-to-tutum'
+import removeFromTutum from './lib/remove-from-tutum'
 import answerToPing from './lib/answer-to-ping'
 
 let debug = require('debug')('tutum-tagger')
@@ -50,6 +51,9 @@ github.user.get({}, function (err, user) {
 
 Bacon.fromEvent(githubEvents, 'push')
   .flatMap((event) => deployToTutum(event, tutum, tutumEvents, github))
+
+Bacon.fromEvent(githubEvents, 'delete')
+  .flatMap((event) => removeFromTutum(event))
 
 Bacon.fromEvent(githubEvents, 'ping')
   .flatMap((event) => answerToPing(event))
