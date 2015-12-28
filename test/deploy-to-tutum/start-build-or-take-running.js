491e605d85bb3f4a9aca0baff2d2791659f8df9b
David Knezić <davidknezic@gmail.com>
2015-12-28 23:02:21 +0100

    Rename build stream, fix it, add tests

---
+++ test/deploy-to-tutum/start-build-or-take-running.js
@@ -0,0 +1,92 @@
+import Bacon from 'baconjs'
+import { expect } from 'chai'
+import { expectation, mock } from 'sinon'
+import startBuildOrTakeRunning from  '../../lib/deploy-to-tutum/start-build-or-take-running'
+
+describe('deploy to tutum', function () {
+  describe('start build or take running', function () {
+    it('it should start a fresh new build that succeeds', function (done) {
+      let buildSetting = {
+        resource_uri: '/api/v1/image/tutum.co/axach/style-guide/buildsetting/latest/',
+        state: 'Success'
+      }
+      let fakeTutum = {}
+      fakeTutum.buildTag = expectation.create('buildTag')
+        .once()
+        .returns(Promise.resolve(buildSetting))
+      let fakeTutumStream = Bacon.once({
+        resource_uri: buildSetting.resource_uri,
+        state: 'Success'
+      })
+
+      startBuildOrTakeRunning(buildSetting, fakeTutum, fakeTutumStream)
+        .onValue((isSuccessful) => {
+          expect(isSuccessful).to.be.true
+          fakeTutum.buildTag.verify()
+          done()
+        })
+    })
+
+    it('it should take a running build that succeeds', function (done) {
+      let buildSetting = {
+        resource_uri: '/api/v1/image/tutum.co/axach/style-guide/buildsetting/latest/',
+        state: 'Building'
+      }
+      let fakeTutum = {}
+      fakeTutum.buildTag = expectation.create('buildTag').never()
+      let fakeTutumStream = Bacon.once({
+        resource_uri: buildSetting.resource_uri,
+        state: 'Success'
+      })
+
+      startBuildOrTakeRunning(buildSetting, fakeTutum, fakeTutumStream)
+        .onValue((isSuccessful) => {
+          expect(isSuccessful).to.be.true
+          fakeTutum.buildTag.verify()
+          done()
+        })
+    })
+
+    it('it should start a fresh new build that fails', function (done) {
+      let buildSetting = {
+        resource_uri: '/api/v1/image/tutum.co/axach/style-guide/buildsetting/latest/',
+        state: 'Success'
+      }
+      let fakeTutum = {}
+      fakeTutum.buildTag = expectation.create('buildTag')
+        .once()
+        .returns(Promise.resolve(buildSetting))
+      let fakeTutumStream = Bacon.once({
+        resource_uri: buildSetting.resource_uri,
+        state: 'Failed'
+      })
+
+      startBuildOrTakeRunning(buildSetting, fakeTutum, fakeTutumStream)
+        .onValue((isSuccessful) => {
+          expect(isSuccessful).to.be.false
+          fakeTutum.buildTag.verify()
+          done()
+        })
+    })
+
+    it('it should take a running build that fails', function (done) {
+      let buildSetting = {
+        resource_uri: '/api/v1/image/tutum.co/axach/style-guide/buildsetting/latest/',
+        state: 'Building'
+      }
+      let fakeTutum = {}
+      fakeTutum.buildTag = expectation.create('buildTag').never()
+      let fakeTutumStream = Bacon.once({
+        resource_uri: buildSetting.resource_uri,
+        state: 'Failed'
+      })
+
+      startBuildOrTakeRunning(buildSetting, fakeTutum, fakeTutumStream)
+        .onValue((isSuccessful) => {
+          expect(isSuccessful).to.be.false
+          fakeTutum.buildTag.verify()
+          done()
+        })
+    })
+  })
+})
