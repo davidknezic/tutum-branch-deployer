4ac5f72483599335255dc34e4d0e0627a237019a
David Knezić <davidknezic@gmail.com>
2016-01-01 20:32:24 +0100

    Implement start service stream, add tests

---
+++ test/deploy-to-tutum/start-service-or-take-running.js
@@ -0,0 +1,106 @@
+import Bacon from 'baconjs'
+import { expect } from 'chai'
+import { expectation } from 'sinon'
+import startServiceOrTakeRunning from  '../../lib/deploy-to-tutum/start-service-or-take-running'
+
+describe('deploy to tutum', function () {
+  describe('start service or take running', function () {
+    it('should start a service that succeeds and has before', function (done) {
+      let service = {
+        resource_uri: '/api/v1/service/74352707-0c01-4b3a-8024-ed2334ad81f2/',
+        state: 'Running'
+      }
+
+      let fakeTutum = {}
+      fakeTutum.startService = expectation.create('startService')
+        .once()
+        .returns(Promise.resolve({
+          resource_uri: service.resource_uri,
+          state: 'Starting'
+        }))
+
+      let fakeTutumStream = Bacon.once({
+        resource_uri: service.resource_uri,
+        state: 'Running'
+      })
+
+      startServiceOrTakeRunning(service, fakeTutum, fakeTutumStream)
+        .onValue((isSuccessful) => {
+          expect(isSuccessful).to.be.true
+          fakeTutum.startService.verify()
+          done()
+        })
+    })
+
+    it('should take a running service start that succeeds', function (done) {
+      let service = {
+        resource_uri: '/api/v1/service/74352707-0c01-4b3a-8024-ed2334ad81f2/',
+        state: 'Starting'
+      }
+
+      let fakeTutum = {}
+      fakeTutum.startService = expectation.create('startService').never()
+
+      let fakeTutumStream = Bacon.once({
+        resource_uri: service.resource_uri,
+        state: 'Running'
+      })
+
+      startServiceOrTakeRunning(service, fakeTutum, fakeTutumStream)
+        .onValue((isSuccessful) => {
+          expect(isSuccessful).to.be.true
+          fakeTutum.startService.verify()
+          done()
+        })
+    })
+
+    it('should start a service that fails and has before', function (done) {
+      let service = {
+        resource_uri: '/api/v1/service/74352707-0c01-4b3a-8024-ed2334ad81f2/',
+        state: 'Stopped'
+      }
+
+      let fakeTutum = {}
+      fakeTutum.startService = expectation.create('startService')
+        .once()
+        .returns(Promise.resolve({
+          resource_uri: service.resource_uri,
+          state: 'Starting'
+        }))
+
+      let fakeTutumStream = Bacon.once({
+        resource_uri: service.resource_uri,
+        state: 'Stopped'
+      })
+
+      startServiceOrTakeRunning(service, fakeTutum, fakeTutumStream)
+        .onValue((isSuccessful) => {
+          expect(isSuccessful).to.be.false
+          fakeTutum.startService.verify()
+          done()
+        })
+    })
+
+    it('should take a running service start that fails', function (done) {
+      let service = {
+        resource_uri: '/api/v1/service/74352707-0c01-4b3a-8024-ed2334ad81f2/',
+        state: 'Starting'
+      }
+
+      let fakeTutum = {}
+      fakeTutum.startService = expectation.create('startService').never()
+
+      let fakeTutumStream = Bacon.once({
+        resource_uri: service.resource_uri,
+        state: 'Stopped'
+      })
+
+      startServiceOrTakeRunning(service, fakeTutum, fakeTutumStream)
+        .onValue((isSuccessful) => {
+          expect(isSuccessful).to.be.false
+          fakeTutum.startService.verify()
+          done()
+        })
+    })
+  })
+})
